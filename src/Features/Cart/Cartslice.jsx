3b5482628917e021f8c2640753c3a32cf3d9ec24
akangbe <akangbehenry28@gmail.com>
2023-11-30 23:43:04 +0100

    fix bug

--- src/Features/Cart/Cartslice.jsx
+++ src/Features/Cart/Cartslice.jsx
@@ -2,6 +2,7 @@ const initialState={
     cart:[{
         pizzaId:12,
         name:"Mediterranean",
+        quatity:2,
         unitPrice:17,
         totalPrice:32,
     }],
@@ -16,9 +17,21 @@ const cartSlice=createslice({
             // Payload=newItem
             state.cart.push(action.payload)
         },
-        deleteItem(state, action){},
-        increaseItemQuatity(state, action){},
-        decreaseItemQuatity(state, action){},
+        deleteItem(state, action){
+            state.cart=state.cart.filter((item)=>item.pizzaId !==action.payload)
+        },
+        increaseItemQuatity(state, action){
+            // payload=pizzaid
+            const item =state.cart.find((item)=>item.pizzaId=== action.payload)
+            item.quatity++
+            item.totalPrice=item.quatity * item.unitPrice
+        },
+        decreaseItemQuatity(state, action){
+              // payload=pizzaid
+              const item =state.cart.find((item)=>item.pizzaId=== action.payload)
+              item.quatity--;
+              item.totalPrice=item.quatity * item.unitPrice
+        },
         clearCart(state, action){},
     }
 })
